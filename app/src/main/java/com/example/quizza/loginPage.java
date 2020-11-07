be312ae67829f3b884d5a2a18840b593c2430a0a
harry <haosun0807@gmail.com>
2020-11-07 15:52:36 +0800

    Work on loginPage and add sample function to achieve login function
    add class_canvas to achieve draw and clear function.

--- app/src/main/java/com/example/quizza/loginPage.java
+++ app/src/main/java/com/example/quizza/loginPage.java
@@ -2,7 +2,14 @@ package com.example.quizza;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class loginPage extends AppCompatActivity {
 
@@ -10,5 +17,25 @@ public class loginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        final EditText usernameEditText = findViewById(R.id.username);
+        final EditText passwordEditText = findViewById(R.id.password);
+        final Button loginButton = findViewById(R.id.login);
+        final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+        loginButton.setEnabled(true);//TODO: add valid data
+        Log.d("Debug","Test username and pwd is Test");
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(usernameEditText.getText().toString().equals("Test")&&passwordEditText.getText().toString().equals("Test"))//TODO:add connect to database and valid the username and password
+                {
+                    Toast.makeText(getApplicationContext(),"Login successful, load to Canvas to test",Toast.LENGTH_LONG).show();
+                    startActivity(new Intent(getApplicationContext(), class_cavans.class));
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),"Login error, please try again",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
